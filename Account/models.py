20aedb5dc68ecffc8431a427c9b880aff837fe50
Sahib Fərhadov <sahibcebrayilli@gmail.com>
2023-08-22 14:15:24 +0400

    deyisiklikler

--- Account/models.py
+++ Account/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 class MyUser(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE)
     bio=models.TextField(max_length=500,blank=True)
-    sekil=models.ImageField("Şəkil",upload_to="main/uploads/%d/%m/%Y",null=True,blank=True,default="rootstatic/img/default-man.jpg")
+    sekil=models.ImageField("Şəkil",upload_to="main/uploads/myuser/images/%d/%m/%Y",null=True,blank=True,default="rootstatic/img/default-man.jpg")
 
     def __str__(self):
         return self.user.username
