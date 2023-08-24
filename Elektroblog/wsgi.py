0a04f172c2f3bdc09bd819eb2fc2ac71fef4b931
SahibFarhadov <SahibCebrayilli@gmail.com>
2023-08-24 20:26:14 +0000

    TIME_ZONE changed to Asia/Baku

--- Elektroblog/wsgi.py
+++ Elektroblog/wsgi.py
@@ -8,6 +8,9 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+import time
+os.environ["TZ"] = "Asia/Baku"
+time.tzset()
 
 from django.core.wsgi import get_wsgi_application
 
